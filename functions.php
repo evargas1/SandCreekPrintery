353f6d9c9bf338620d7eb156a9f11f86846777a6
Estrella Vargas <estrellavargas613@gmail.com>
2023-11-09 16:00:37 -0500

    attached styling sheets

--- functions.php
+++ functions.php
@@ -1,5 +1,10 @@
 <?php
+wp_enqueue_style( 'general', get_template_directory_uri('/css/general.css'));
 
-
-
+function theme_styles(){ 
+    // Load all of the styles that need to appear on all pages
+    wp_enqueue_style( 'general', get_template_directory_uri() . 'css/general.css' );
+    
+    }
+     add_action('wp_enqueue_scripts', 'theme_styles');
 ?>
